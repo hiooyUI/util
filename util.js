f747ebbe06ac3ca4a8b387396ba9ab4364203efd
Nelson Kuang <410374308@qq.com>
2016-09-09 16:30:57 +0800

    增加addEvent函数

--- util.js
+++ util.js
@@ -249,3 +249,49 @@ function goTop(acceleration, time) {
     }
 };
 
+/**
+ * 简易的事件添加方法
+ * http://www.zhangxinxu.com/wordpress/2013/04/js-mousewheel-dommousescroll-event/
+ * 
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ */
+window.addEvent = (function (window, undefined) {
+    var _eventCompat = function (event) {
+        var type = event.type;
+        if (type == 'DOMMouseScroll' || type == 'mousewheel') {
+            event.delta = (event.wheelDelta) ? event.wheelDelta / 120 : -(event.detail || 0) / 3;
+        }
+        if (event.srcElement && !event.target) {
+            event.target = event.srcElement;
+        }
+        if (!event.preventDefault && event.returnValue !== undefined) {
+            event.preventDefault = function () {
+                event.returnValue = false;
+            };
+        }
+        /* 
+           ......其他一些兼容性处理 */
+        return event;
+    };
+    if (window.addEventListener) {
+        return function (el, type, fn, capture) {
+            if (type === "mousewheel" && document.mozHidden !== undefined) {
+                type = "DOMMouseScroll";
+            }
+            el.addEventListener(type, function (event) {
+                fn.call(this, _eventCompat(event));
+            }, capture || false);
+        }
+    } else if (window.attachEvent) {
+        return function (el, type, fn, capture) {
+            el.attachEvent("on" + type, function (event) {
+                event = event || window.event;
+                fn.call(el, _eventCompat(event));
+            });
+        }
+    }
+    return function () { };
+})(window);
