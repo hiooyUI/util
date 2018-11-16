4c076544dfb3f4fcf7067390c7e7ac3c12701968
Nelson Kuang <410374308@qq.com>
2018-11-16 14:21:44 +0800

    Update util-es6.js

--- util-es6.js
+++ util-es6.js
@@ -5,7 +5,7 @@
  * @return {String}
  */
 export const randomMobile = function (withMask) {
-  const startCodeArr = [139, 138, 137, 136, 135, 134, 159, 158, 157, 150, 151, 152, 188, 187, 182, 183, 184, 178, 130, 131, 132, 156, 155, 186, 185, 176, 133, 153, 189, 180, 181, 177, 173]
+  const startCodeArr = [139, 138, 137, 136, 135, 134, 159, 158, 157, 150, 151, 152, 188, 187, 182, 183, 184, 178, 130, 131, 132, 156, 155, 186, 185, 176, 133, 153, 189, 180, 181, 177, 173, 199, 198]
   const areaCode = withMask ? 'xxxx' : String(new Date().valueOf()).substr(-4, 4)
   const userCode = String(new Date().getTime() + Math.floor(9999 * Math.random())).substr(-4, 4)
   const startCode = String(startCodeArr[Math.floor((startCodeArr.length - 1) * Math.random())])
