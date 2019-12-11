f8d1c80676165ce0fba03dfe5c89286cc6f30e85
雨夜带铲 Fill the endless holes~ <410374308@qq.com>
2019-12-11 15:53:37 +0800

    Update util-es6.js

--- util-es6.js
+++ util-es6.js
@@ -36,3 +36,66 @@ export const deepEq = (a, b) => {
   if (type !== 'function' && type !== 'object' && typeof b != 'object') return false
   return JSON.stringify(a) === JSON.stringify(b)
 }
+
+/**
+ * canvas 换行画出多行文字
+ * @param  {CanvasRenderingContext2D | WebGLRenderingContext | WebGL2RenderingContext | ImageBitmapRenderingContext} ctx
+ * @param  {String} text
+ * @param  {Number} x
+ * @param  {Number} y
+ * @param  {Number} w，即一行的宽度 width
+ * @param  {String} color，即字体的颜色
+ * @param  {Number} fontSize
+ * @param  {Boolean} isBold，即是否加粗
+ */
+export function drawMultiLineRichText (ctx, text, x, y, w, color, fontSize, isBold) {
+  if (!text) {
+    return false
+  }
+  const rawRows = text.split('\n')
+  let rows = []
+  // 保存画布 用 save 及 restore 是为了不影响其他地方使用画布
+  ctx.save()
+  fontSize = fontSize ? fontSize : 26
+  ctx.font = `${isBold ? '700' : '400'} ${fontSize}px "Hiragino Sans GB W3","Microsoft YaHei",sans-serif`
+  ctx.textBaseline = 'middle'
+  ctx.fillStyle = color
+  rawRows.forEach((txt) => {
+    const chars = txt.split('')
+    let tempStr = ''
+    chars.forEach((char) => {
+      if (ctx.measureText(tempStr).width >= w) {
+        rows.push(tempStr)
+        tempStr = ''
+      }
+      tempStr += char
+    })
+    rows.push(tempStr)
+  })
+  rows.forEach((txt, n) => {
+    ctx.fillText(txt, x, y + n * fontSize * 1.8) // ****** 使用 FILLTEXT
+  })
+  ctx.restore() // 恢复画布
+}
+
+/**
+ * 预加载图片
+ * @param  {String} url，图片地址
+ * @param  {String} callback，图片加载完成的回调函数
+ * @param  {Boolean} needCrossOrigin，是否跨域
+ * @returns {Image}，返回自己本身
+ */
+export function preLoadImage (url, callback, needCrossOrigin) { // fixed Chrome 图片load 不出来问题
+  let img = new Image() // 创建一个 Image 对象，实现图片的预下载
+  needCrossOrigin && img.crossOrigin = 'anonymous'
+  img.src = url
+
+  if (img.complete) { // 如果图片已经存在于浏览器缓存，直接调用回调函数  
+    callback && callback.call(img)
+    return // 直接返回，不用再处理 onload 事件  
+  }
+
+  img.onload = function () { // 图片下载完毕时异步调用 callback 函数。  
+    callback && callback.call(img) // 将回调函数的 this 替换为 Image 对象  
+  }
+}
