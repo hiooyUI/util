e9342aaa61d71f90b4fd34f7b827296d5a55bcae
雨夜带铲 - Fill the endless holes~ <410374308@qq.com>
2020-06-11 09:11:07 +0800

    Update util.js

--- util.js
+++ util.js
@@ -482,6 +482,28 @@ var simpleThrottle = function (fn, delay) {
     }, delay);
 }
 
+// Throttle 简化版 II
+function simpleThrottleII(func, wait, mustRun) {
+        var timeout,
+                startTime = new Date();
+
+        return function() {
+                var context = this,
+                        args = arguments,
+                        curTime = new Date();
+
+                clearTimeout(timeout);
+                // 如果达到了规定的触发时间间隔，触发 handler
+                if(curTime - startTime >= mustRun){
+                        func.apply(context,args);
+                        startTime = curTime;
+                        // 没达到触发间隔，重新设定定时器
+                }else{
+                        timeout = setTimeout(func, wait);
+                }
+        };
+};
+
 /**
   * Deep clones your object and returns a new object.
   */
